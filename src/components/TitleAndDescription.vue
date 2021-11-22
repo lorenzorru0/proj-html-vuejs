5259e94bf79a09e5c692bb150d428ed0cc61e625
lorenzorru0 <lorenzoorru999@gmail.com>
2021-11-22 10:37:11 +0100

    Add fix

---
+++ src/components/TitleAndDescription.vue
@@ -0,0 +1,40 @@
+<template>     
+    <div>         
+        <h3>{{data.title}}</h3>         
+        <div class="hr"></div>         
+        <p>{{data.description}}</p>     
+    </div> 
+</template>  
+
+<script> 
+    export default {     
+        name: 'TitleAndDescription',     
+        props: ['data'] } 
+</script>  
+
+<style lang="scss" scoped> 
+@import '../assets/style/variable.scss';  
+
+div {     
+    .hr {         
+        margin: auto;         
+        width: 12.5rem;         
+        height: .125rem;         
+        background-color: $commonYellow;         
+        margin: {             
+            top: 1.25rem;             
+            bottom: 1.875rem;         
+        };     
+    }      
+    
+    h3 {         
+        text-align: center;         
+        font-weight: lighter;     
+    }      
+    
+    p {         
+        line-height: 2rem;         
+        color: #747474;     
+    } 
+}  
+</style>
